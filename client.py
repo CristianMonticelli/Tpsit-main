852de947427c4d03a675ba1efb86503c5bac3b19
CristianMonticelli <cristian.monticelli@studenti.isissgobetti.it>
2026-02-12 11:31:59 +0100

    Esercizio 1 - Creare un client TCP in Python

--- client.py
+++ client.py
@@ -1,24 +1,24 @@
 import socket
 
 def main():
-    # 1️⃣ Creazione del socket TCP
+    # Creazione del socket TCP
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     server_name = "localhost"
     server_port = 6789
 
     try:
-        # 2️⃣ Connessione al server
+        #  Connessione al server
         client_socket.connect((server_name, server_port)) #java new Socket(nomeServer, portaServer);
         print("CLIENT connesso al server")
 
-        # 3️⃣ Input da tastiera
+        # Input da tastiera
         messaggio = input("Inserisci la stringa da inviare al server: ")
 
-        # 4️⃣ Invio al server (serve convertire in byte)
+        # Invio al server (serve convertire in byte)
         client_socket.send(messaggio.encode())
 
-        # 5️⃣ Ricezione risposta dal server
+        # Ricezione risposta dal server
         risposta = client_socket.recv(1024).decode()
         print("Risposta dal server:", risposta)
 
@@ -26,7 +26,7 @@ def main():
         print("Errore durante la comunicazione:", e)
 
     finally:
-        # 6️⃣ Chiusura connessione
+        # Chiusura connessione
         client_socket.close()
         print("Connessione chiusa")
 
